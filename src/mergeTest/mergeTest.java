27048d2301c94dc3fdcbe6b09abfc818526bebe8
cofitost <zxcv091922@gmail.com>
2018-11-26 14:36:36 +0800

    first branch

--- src/mergeTest/mergeTest.java
+++ src/mergeTest/mergeTest.java
@@ -5,6 +5,7 @@ public class mergeTest {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.print("Hello World!");
+    System.out.print("first branch");
   }
 
 }
